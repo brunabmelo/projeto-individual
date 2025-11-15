c582d8771d8432712a9b24da40e892f2bd5f7ddc
brunabmelo <bruna.bmelo@sptech.school>
2025-11-15 15:30:41 -0300

    Ajuste (login|Cadastro|index): conecção com as paginas e delet de paginas antigas

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,62 +1,43 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+CREATE DATABASE myeevee;
+USE myeevee;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	idusuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+    sobrenome VARCHAR(100),
+	email VARCHAR(100),
+	senha VARCHAR(8)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE mascote (
+	idmascote INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+	nivel INT,
+	fkusuario INT,
+    CONSTRAINT FkUsuarioMascote
+		FOREIGN KEY (fkusuario)
+		REFERENCES usuario(idusuario)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE evolucao (
+	idevolucao INT PRIMARY KEY AUTO_INCREMENT,
+	acao VARCHAR(45),
+	xp INT,
+    dtHora DATETIME,
+	fkmascote INT,
+    CONSTRAINT FkEvolucaoMascote
+		FOREIGN KEY (fkmascote)
+		REFERENCES mascote(idmascote)
 );
+INSERT INTO usuario (nome, sobrenome, email, senha) VALUES
+('Adm', 'Teste', 'adm@teste.com', 1234);
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+INSERT INTO mascote (nome, nivel, fkusuario) VALUES
+('eevee', 1, 1);
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+SELECT u.*,
+m.*
+FROM usuario u
+JOIN mascote m
+ON idusuario = fkusuario;
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
