f095cc413c8f3ff7f5efe6d443e86bc7ca08c0f8
brunabmelo <bruna.bmelo@sptech.school>
2025-11-17 09:13:23 -0300

    Modificação(index): Modificação da estrutura do site

---
+++ src/models/eeveeModel.js
@@ -0,0 +1,29 @@
+var database = require("../database/config");
+
+function autenticar(idusuario) {
+    console.log("ACESSEI O MASCOTE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT idusuario, nome, sobrenome, email  FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, nivel, idusuario) {
+    console.log("ACESSEI O MASCOTE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, nivel, idusuario);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO mascote (nome, nivel, senha) VALUES ('Eevee','0', '${idusuario}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
