a7e68b96c21b77b2b15abb52d29b5b3d6def87dd
brunabmelo <bruna.bmelo@sptech.school>
2025-11-21 09:51:55 -0300

    Modificação(grafico): tamanho ajustado

--- src/routes/grafico.js
+++ src/routes/grafico.js
@@ -3,8 +3,14 @@ var router = express.Router();
 
 var graficoController = ("../controllers/graficoController");
 
+//Recebendo os dados graficoController.js
+router.post("/cadastrar", function (req, res) {
+    graficoController.cadastrar(req, res);
+})
+
 router.get("/grafico", function (req, res) {
     graficoController.grafico (req, res);
-})
+});
+
 module.exports = router;
 
